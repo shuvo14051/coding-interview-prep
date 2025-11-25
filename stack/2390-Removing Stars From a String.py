37418dbee3f805e23a060d362ff270ca092a52e3
Md Younus Ahamed <42135612+shuvo14051@users.noreply.github.com>
2025-11-24 19:35:11 -0500

    Leetcode 2390 (Stack)

--- stack/2390-Removing Stars From a String.py
+++ stack/2390-Removing Stars From a String.py
@@ -1,4 +1,27 @@
-# https://leetcode.com/problems/removing-stars-from-a-string/description/
+"""
+LeetCode 2390: Removing Stars From a String 
+https://leetcode.com/problems/removing-stars-from-a-string/description/
+
+Description:
+You are given a string s, which contains stars *.
+In one operation, you can:
+- Choose a star in s.
+- Remove the closest non-star character to its left, as well as remove the star itself.
+Return the string after all stars have been removed.
+Note:
+- The input will be generated such that the operation is always possible.
+- It can be shown that the resulting string will always be unique.
+
+Example 1:
+Input: s = "leet**cod*e"
+Output: "lecoe"
+
+Example 2:
+Input: s = "erase*****"
+Output: ""
+"""
+
+
 class Solution:
     def removeStars(self, s: str) -> str:
         stack = []
@@ -10,13 +33,14 @@ class Solution:
 
         return ''.join(stack)
     
-
-    # def removeStars(self, s: str) -> str:
-    #     stack = []
-    #     for ch in s:
-    #         if ch == '*':
-    #             if stack:
-    #                 stack.pop()
-    #         else:
-    #             stack.append(ch)
-    #     return ''.join(stack)
+"""
+    def removeStars(self, s: str) -> str:
+        stack = []
+        for ch in s:
+            if ch == '*':
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(ch)
+        return ''.join(stack)
+"""
